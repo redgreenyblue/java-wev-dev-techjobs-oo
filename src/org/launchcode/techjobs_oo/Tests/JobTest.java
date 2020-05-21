3f7f9a3b5e0c91e85355ca8b8f67b0916a550562
Rodney Halbert <rodneyhalbert@rocketmail.com>
2020-05-21 15:01:07 -0500

    Test equals method

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -11,12 +11,16 @@ public class JobTest {
     Job jobObjOne;
     Job jobObjTwo;
     Job jobObjThree;
+    Job jobObjFour;
+    Job jobObjFive;
 
     @Before
     public void jobTestObj() {
         jobObjOne = new Job("test job", new Employer("test emp"), new Location("test loc"), new PositionType("test pos"), new CoreCompetency("test comp"));
         jobObjTwo = new Job("abc job", new Employer("abc emp"), new Location("abc loc"), new PositionType("abc pos"), new CoreCompetency("abc comp"));
         jobObjThree = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        jobObjFour = new Job("Jobby Job", new Employer("Master Employer"), new Location("Anywhere"), new PositionType("Worker Bee"), new CoreCompetency("Obedience"));
+        jobObjFive = new Job("Jobby Job", new Employer("Master Employer"), new Location("Anywhere"), new PositionType("Worker Bee"), new CoreCompetency("Obedience"));
     }
 
     @Test
@@ -31,5 +35,10 @@ public class JobTest {
         assertTrue(jobObjThree.getLocation().getValue().equals("Desert"));
         assertTrue(jobObjThree.getPositionType().getValue().equals("Quality control"));
         assertTrue(jobObjThree.getCoreCompetency().getValue().equals("Persistence"));
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        assertFalse(jobObjFour == jobObjFive);   
     }
 }
