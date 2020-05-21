d73728ac6698d0224b3e98a56cd60fefe23008fc
Rodney Halbert <rodneyhalbert@rocketmail.com>
2020-05-21 17:27:07 -0500

    Extended classes to JobField base class

--- src/org/launchcode/techjobs_oo/JobField.java
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -22,8 +22,10 @@ public abstract class JobField {
         return value;
     }
 
+    // Custom toString, equals, and hashCode methods:
+
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) {   // Two objects are equal if they have the same id.
         if (this == o) return true;
         if (!(o instanceof JobField)) return false;
         JobField jobField = (JobField) o;
